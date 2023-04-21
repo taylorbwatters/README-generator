4c2182e2669c12e0633f49de9acc0094277cecb9
Taylor Watters <taylorbbigelow@gmail.com>
2023-04-20 22:30:16 -0700

    fixes bad question

--- Develop/index.js
+++ Develop/index.js
@@ -37,7 +37,7 @@ const questions = [
    },
    {
        name:'tests',
-       message:'provide examples on how to run them here',
+       message:'provide examples on how to run tests',
        type:'input',
    },
    {
